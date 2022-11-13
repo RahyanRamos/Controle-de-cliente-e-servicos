6e9c8a620771a4ac0c10176550242b6f2b3636db
Rahyan Ramos <rahyanramos@gmail.com>
2022-11-13 17:42:19 -0400

    Criação do formulário para cadastro da Ordem de Serviço

--- Projeto/GerOrdem.php
+++ Projeto/GerOrdem.php
@@ -15,7 +15,26 @@
     </header>
     <?php include_once '_parts/_header.php'; ?>
     <div class="container mt-3">
-
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+            <div class="row">
+                <div class="col-md-2">
+                    <label for="txtNumero" class="form-label">Nº Ordem de Serviço</label>
+                    <input type="text" name="txtNumero" id="txtNumero" readonly class="form-control">
+                </div>
+                <div class="col-md-2">
+                    <label for="txtData" class="form-label">Data</label>
+                    <input type="date" name="txtData" id="txtData" value="<?php echo date(Y-m-d) ?>" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <label for="sltCliente" class="form-label"></label>
+                    <select name="sltCliente" id="sltCliente" class="form-select">
+                        <option value="">Selecione o cliente</option>
+                    </select>
+                </div>
+            </div>
+        </form>
     </div>
     <?php include '_parts/_linkJS.php'; ?>
 </body>
